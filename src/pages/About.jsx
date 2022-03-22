ea25181e6da3124c3f1cc66876b4ca446cdcedee
MihailNovikow <mike10102000@gmail.com>
2022-03-22 10:41:28 +0300

    Initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Приложение "Список постов"
+        </h1>
+    );
+};
+
+export default About;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{color: 'red'}}>
+                Страница не найдена!
+            </h1>
+        </div>
+    );
+};
+
+export default Error;
